07145c4004ccb755a9ad202039404a28c8ac5598
Harsh Deep <hrshdeep51@gmail.com>
2025-02-19 01:14:43 -0500

    sending messages working

--- src/Chatroom.svelte
+++ src/Chatroom.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
-  import type { RxStomp } from "@stomp/rx-stomp"
+  import { RxStomp } from "@stomp/rx-stomp"
+  import type { ChatMessage } from "./types"
 
   export let rxStomp: RxStomp
 
   let message = ''
   let userName = `user${Math.floor(Math.random() * 1000)}`
+
+  const CHATROOM_NAME = '/topic/test'
+  
+  function sendMessage(chatMessage: ChatMessage) {
+    const body = JSON.stringify(chatMessage)
+    rxStomp.publish({ destination: CHATROOM_NAME, body })
+    console.log(`Sent ${body}`)
+    message = ''
+  }
 </script>
 
 <h2>Chatroom</h2>
@@ -14,3 +24,5 @@
 
 <label for="message">Message:</label>
 <input type="text" bind:value={message} />
+
+<button on:click={() => sendMessage({ userName, message })}>Send Message</button>
