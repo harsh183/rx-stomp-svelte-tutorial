ed5e82bf6b1cae2d252485018df1e45c1f86a86b
Harsh Deep <hrshdeep51@gmail.com>
2025-02-19 01:41:36 -0500

    sending messages

--- src/Messages.svelte
+++ src/Messages.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import { RxStomp } from "@stomp/rx-stomp"
   import type { ChatMessage } from "./types"
+  import { onMount } from "svelte"
+  import { map } from "rxjs/operators"
 
   export let rxStomp: RxStomp
   export let topic: string
@@ -8,6 +10,15 @@
   let chatMessages: ChatMessage[] = [
     { userName: "admin", message: `Welcome to ${topic} room!`}
   ]
+
+  onMount(() => {
+    const messageSubscription = rxStomp
+      .watch(topic)
+      .pipe(map((payload) => JSON.parse(payload.body)))
+      .subscribe((chatMessage) => chatMessages = [...chatMessages, chatMessage])
+
+    return () => messageSubscription.unsubscribe()
+  })
 </script>
 
 <h2>Messages</h2>
